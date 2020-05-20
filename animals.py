b0eea0802b24236e2877be3071e16db9be617074
Daniel Schetelig <d.schetelig@schetelig.de>
2020-05-20 10:50:16 +0200

    added basic functionality

---
+++ animals.py
@@ -0,0 +1,13 @@
+# Script to return the sound of an animal based on their name
+# Input: animal name
+# Output: animal sound
+
+import sys
+
+def default():
+    print('Hello')
+
+def main():
+    default()
+
+main()
