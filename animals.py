0311ccc533194514535a0e85d3ae1f27c80e6f6e
Daniel Schetelig <d.schetelig@schetelig.de>
2020-05-20 11:06:46 +0200

    added cat func

--- animals.py
+++ animals.py
@@ -3,11 +3,15 @@
 # Output: animal sound
 
 import sys
+from cat import cat
 
 def default():
     print('Hello')
 
 def main():
-    default()
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
 
 main()
